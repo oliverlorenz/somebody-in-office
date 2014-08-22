f9e179d0ce8d3d3db9210111c69bdc51ebe03bf8
Oliver Lorenz <oliver.lorenz@antevorte.org>
2014-08-22 09:39:58 +0200

    better logic implemented

--- check.php
+++ check.php
@@ -16,39 +16,54 @@ if (empty($argv)) {
 function activate()
 {
     echo "activate!\n";
-    system('export $( grep -ao "DBUS_SESSION_BUS_ADDRESS=[^\0]*"  /proc/$(pidof -s gnome-screensaver)/environ ); gnome-screensaver-command -d');
+    // system('export $( grep -ao "DBUS_SESSION_BUS_ADDRESS=[^\0]*"  /proc/$(pidof -s gnome-screensaver)/environ ); gnome-screensaver-command -d');
 }
 
 function deactivate()
 {
     echo "deactivate!\n";
-    system('export $( grep -ao "DBUS_SESSION_BUS_ADDRESS=[^\0]*"  /proc/$(pidof -s gnome-screensaver)/environ ); gnome-screensaver-command -a');
+    // system('export $( grep -ao "DBUS_SESSION_BUS_ADDRESS=[^\0]*"  /proc/$(pidof -s gnome-screensaver)/environ ); gnome-screensaver-command -a');
 }
 
 $sortedList = $argv;
 while(true) {
 
-    $somebodyIsHome = false;
-    foreach ($sortedList as $index => $address) {
-        echo "Look! In ". $address;
-
-        $pingAddress = $sortedList[$index];
-        unset($sortedList[$index]);
-        $output = array();
-
-        @exec("ping -c 1 -W 20 $pingAddress", $output, $status);
-        if(isset($output[4]) && preg_match('/[1-9]+ received/', $output[4])) {
-            $somebodyIsHome = true;
-            echo "'s window is light!\n";
-            array_unshift($sortedList, $pingAddress);
-            break;
-        } else {
-            $somebodyIsHome = false;
-            echo " is not home\n";
-            array_push($sortedList, $pingAddress);
+    $countOfPeopleInOffice = 0;
+    $newSortedList = array();
+    foreach ($sortedList as $index => $pingAddress) {
+    	$output = array();
+		echo $pingAddress . ' ';
+        
+        $thereIsSomebody = false;
+        $command = "ping -c 1 -W 1 $pingAddress";
+        // echo "\n$command\n";
+    	@exec("ping -c 1 -W 1 $pingAddress", $output, $status);
+        foreach ($output as $line) {
+        	if(preg_match('/[1-9]+ received/', $line)) {
+    			$thereIsSomebody = true;
+        	}
         }
+
+        if ($thereIsSomebody) {
+        	array_unshift($newSortedList, $pingAddress);
+        	foreach ($sortedList as $value) {
+        		if ($value != $pingAddress) {
+        			array_push($newSortedList, $value);
+        		}
+        	}
+        	$countOfPeopleInOffice++;
+        	echo 'is in office';
+        	echo "\n";
+        	break;
+        } else {
+        	array_push($newSortedList, $pingAddress);
+        	echo 'is not in office';
+        	echo "\n";
+        }
+        
     }
-    if ($somebodyIsHome) {
+    $sortedList = $newSortedList;
+    if (!empty($countOfPeopleInOffice)) {
         activate();
     } else {
         deactivate();
